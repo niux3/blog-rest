d2e3cae84f79198e881cd924cc78e70e42c09463
niuxe <renaudbourdeau@gmail.com>
2023-09-16 15:36:10 +0200

    feat: clean file

--- app/account/views.py
+++ app/account/views.py
@@ -1,8 +1,10 @@
+import hashlib
 from flask_restful import Resource
 from flask import request
 from app.account.models import User
-from app.account.forms import RegisterForm
+from app.account.forms import RegisterForm, LoginForm
 from app.db import db
+from app.config import Config
 
 
 class Users(Resource):
@@ -21,26 +23,50 @@ class Users(Resource):
         ]
 
     def post(self):
-        data = request.get_json()
-        if data['type'] == 'register':
-            form = RegisterForm(data=data)
-            if form.validate():
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            data = request.get_json()
+            if data['type'] == 'register':
                 del data['type']
-                del data['confirm']
-                user = User(**data)
-                db.session.add(user)
-                db.session.commit()
+                form = RegisterForm(data=data)
+                if form.validate():
+                    del data['confirm']
+                    user = User(**data)
+                    db.session.add(user)
+                    db.session.commit()
+                    return {
+                        'message': 'ok'
+                    }, 201
+                else:
+                    return {
+                        "errors": form.errors
+                    }, 400
+            elif data['type'] == 'login':
+                del data['type']
+                form = LoginForm(data=data)
+                if form.validate():
+                    chaine_password = Config.SALT + data['password']
+                    password = hashlib.md5(chaine_password.encode('utf8')).hexdigest()
+                    row = db.session.query(User).filter_by(email=data['email'], password=password).first()
+                    if row is None:
+                        return {
+                            "errors": {
+                                "email": ["l'email ou le mot de passe ne correspondent pas"]
+                            }
+                        }
+                    else:
+                        return {
+                            'message': 'ok',
+                            'username': row.username,
+                            'id': row.id,
+                            'logged': True
+                        }
+                else:
+                    return {
+                        "errors": form.errors
+                    }, 400
                 return {
-                    'message': 'ok'
+                    'message': 'login ok'
                 }, 201
-            else:
-                return {
-                    "errors": form.errors
-                }, 400
-        elif data['type'] == 'login':
-            return {
-                'message': 'login ok'
-            }, 201
         return {
             'message': 'ko'
         }, 404
